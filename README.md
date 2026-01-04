7d97fc2cf5e7b803c5cd33729ed60b54544c8458
Oliver Ford <dev@ojford.com>
2026-01-04 23:07:22 +0000

    Support restricting subnets forwarded

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 tailscale/tailscale, plus forwarding DNS queries on the local interface to the tailnet (MagicDNS, and whatever is configured).
 
 All env vars etc. as per the upstream image; defaults unchanged except `TS_ACCEPT_DNS=1` (it doesn't make sense to decline it) and the addition of:
+- `ALLOW_FROM_ADDR_V4`: e.g. `192.168.88.0/24,172.1.2.3`, restrict forwarding only to given addresses/subnets
+- `ALLOW_FROM_ADDR_V6`: e.g. `::dead,::beef`, as above but ipv6
 - `HOST_INTERFACE`: e.g. `eth0`, the LAN interface name
 
 SSHD is also enabled in order that `TS_EXTRA_ARGS=--ssh` can be used, but this is not set by default.
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,25 @@ cmd="$@"
 
 dnsmasq --no-daemon --interface="$HOST_INTERFACE" &
 
-# necessary to allow devices behind the subnet router to reach the tailnet with SNAT
-# https://github.com/tailscale/tailscale/pull/18318
-iptables -t mangle -A FORWARD -i "$HOST_INTERFACE" -o tailscale0 -j MARK --set-xmark 0x40000/0xff0000
-ip6tables -t mangle -A FORWARD -i "$HOST_INTERFACE" -o tailscale0 -j MARK --set-xmark 0x40000/0xff0000
+# MARK necessary to allow devices behind the subnet router to reach the
+# tailnet with SNAT: https://github.com/tailscale/tailscale/pull/18318
+chain=('--table=mangle' '--insert=FORWARD')
+if_cond=("--in-interface=$HOST_INTERFACE" '--out-interface=tailscale0')
+# optionally restrict to the given $ALLOW_FROM_ADDR_V4/6 (e.g. it (V4)
+# may be the whole $TS_ROUTES, or it could be completely disjoint.
+src_cond4=()
+src_cond6=()
+if [ -n "${ALLOW_FROM_ADDR_V4-}" ]; then
+    src_cond4=("--source=$ALLOW_FROM_ADDR_V4")
+    iptables "${chain[@]}" ! "${src_cond4[@]}" "${if_cond[@]}" --jump=DROP
+fi
+if [ -n "${ALLOW_FROM_ADDR_V6-}" ]; then
+    src_cond6=("--source=$ALLOW_FROM_ADDR_V6")
+    ip6tables "${chain[@]}" ! "${src_cond6[@]}" "${if_cond[@]}" --jump=DROP
+fi
+
+mark=('--jump=MARK' '--set-xmark=0x40000/0xff0000')
+iptables "${chain[@]}" "${src_cond4[@]}" "${if_cond[@]}" "${mark[@]}"
+ip6tables "${chain[@]}" "${src_cond6[@]}" "${if_cond[@]}" "${mark[@]}"
 
 exec "${cmd[@]}"
